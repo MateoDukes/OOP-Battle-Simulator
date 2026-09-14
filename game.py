caeddc8d238ee119e61b92a48506376aa0b0a435
MateoDukes <mateokelsondukes@gmail.com>
2026-09-14 10:46:51 -0500

    Build Hero Class

--- game.py
+++ game.py
@@ -1,14 +1,14 @@
 from goblin import Goblin
-
+from hero import Hero
 
 ARENA_NAME = "The Golden Triangle"
-
 
 def main():
     """Open the arena and introduce its first opponent."""
     print(f"Welcome to {ARENA_NAME}!")
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
     print("The gates are opening...")
+    print()
 
     goblin = Goblin("Prapple")
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
@@ -16,8 +16,20 @@ def main():
     goblin2 = Goblin("Ploop")
     print(f"{goblin2.name} enters the arena with {goblin2.health} health.")
 
-    print("But no hero has answered the call... yet.")
+    hero = Hero("Mateo")
+    print(f"{hero.name} enters the arena with {hero.health} health.")
 
+    print()
+    attack = hero.attack()
+    goblin.take_damage(attack)
+    print(f"{hero.name} attacks {goblin.name} and deals {attack} damage! {goblin.name} has {goblin.health} health remaining.")
+
+    if goblin.is_alive != True:
+        print()
+        attack = goblin.attack()
+        hero.take_damage(attack)
+        print(f"{goblin.name} attacks {hero.name} and deals {attack} damage! {hero.name} has {hero.health} health remaining.")
+        print(f"{hero.name} - '{hero.battle_cry()}'")
 
 if __name__ == "__main__":
     main()
--- hero.py
+++ hero.py
@@ -1,4 +1,32 @@
-class Hero:
-    """The hero blueprint will be implemented later in the project."""
+import random
 
-    pass
+class Hero:
+    def __init__(self, name):
+        self.name = name
+        self.health  = 150
+        self.attack_power = 15
+
+    def attack(self):
+        return random.randint(1, self.attack_power)
+
+    def take_damage(self, damage):
+        self.health = max(0, self.health - damage)
+
+    def is_alive(self):
+        return self.health > 0
+
+    def battle_cry(self):
+        if self.health > 110 and self.health <= 150:
+            return "Your attack is a mere scar! I will defeat you!"
+        
+        elif self.health > 70 and self.health <= 110:
+            return "You might have damaged me, but I am still strong!"
+
+        elif self.health > 30 and self.health <= 70:
+            return "Aagh! I have been deeply wounded, but I'm not dead yet!"
+
+        elif self.health > 0 and self.health <= 30:
+            return "AAGH!!! You dastardly devil! I will not be vanquished!"
+
+        else:
+            return "I have been defeated! Aagh..."
