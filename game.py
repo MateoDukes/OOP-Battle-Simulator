f9d242759014328478e8267fe08215b902edc360
MateoDukes <mateokelsondukes@gmail.com>
2026-09-15 10:15:36 -0500

    game-loop.py

--- game.py
+++ game.py
@@ -1,10 +1,74 @@
 from goblin import Goblin
 from hero import Hero
+import time
 
 ARENA_NAME = "The Golden Triangle"
 
+def battle(protag, antag):
+    round = 1
+
+    if protag.is_alive() and antag.is_alive():
+        living = True
+    else:
+        living = False
+
+    while living:
+    
+        print()
+        time.sleep(1)
+        print(f"{protag.name} battles {antag.name} - ROUND {round}")
+        time.sleep(1)
+        print()
+
+        for i in range(1):
+            print(f"\r{protag.name} is attacking", end="")
+            time.sleep(0.5)
+            print(f"\r{protag.name} is attacking.", end="")
+            time.sleep(0.5)
+            print(f"\r{protag.name} is attacking..", end="")
+            time.sleep(0.5)
+            print(f"\r{protag.name} is attacking...", end="")
+            time.sleep(0.5)
+        print()
+        
+        attack = protag.attack()
+        antag.take_damage(attack)
+        print(f"{protag.name} attacks {antag.name} and deals {attack} damage! {antag.name} has {antag.health} health remaining.")
+
+        if antag.is_alive():
+            print()
+            for i in range(1):
+                print(f"\r{antag.name} is attacking", end="")
+                time.sleep(0.5)
+                print(f"\r{antag.name} is attacking.", end="")
+                time.sleep(0.5)
+                print(f"\r{antag.name} is attacking..", end="")
+                time.sleep(0.5)
+                print(f"\r{antag.name} is attacking...", end="")
+                time.sleep(0.5)
+            print()
+
+            attack = antag.attack()
+            protag.take_damage(attack)
+            print(f"{antag.name} attacks {protag.name} and deals {attack} damage! {protag.name} has {protag.health} health remaining.")
+            print(f"{protag.name} - '{protag.battle_cry()}'")
+
+        round += 1
+        time.sleep(2)
+
+        if protag.is_alive() and antag.is_alive():
+            living = True
+        else:
+            if not protag.is_alive():
+                print()
+                print(f"{protag.name} has been defeated by {antag.name}!")
+            elif not antag.is_alive():
+                print()
+                print(f"{antag.name} has been defeated by {protag.name}!")
+            
+            living = False
+
 def main():
-    """Open the arena and introduce its first opponent."""
     print(f"Welcome to {ARENA_NAME}!")
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
     print("The gates are opening...")
@@ -19,17 +83,7 @@ def main():
     hero = Hero("Mateo")
     print(f"{hero.name} enters the arena with {hero.health} health.")
 
-    print()
-    attack = hero.attack()
-    goblin.take_damage(attack)
-    print(f"{hero.name} attacks {goblin.name} and deals {attack} damage! {goblin.name} has {goblin.health} health remaining.")
-
-    if goblin.is_alive != True:
-        print()
-        attack = goblin.attack()
-        hero.take_damage(attack)
-        print(f"{goblin.name} attacks {hero.name} and deals {attack} damage! {hero.name} has {hero.health} health remaining.")
-        print(f"{hero.name} - '{hero.battle_cry()}'")
+    battle(hero, goblin)
 
 if __name__ == "__main__":
     main()
